53d8f6b1289817a3bf84393056b89bd8d978d08a
Lana Vu <lananghivu@gmail.com>
2026-01-02 03:59:03 -0800

    built POKEBOWL class a bit, not finalized

--- SuperPoke/SuperPoke.hpp
+++ SuperPoke/SuperPoke.hpp
@@ -11,10 +11,41 @@
 
 void pokePath();
 
-class pokeBowl {
+class pokeBowl { //making a bowl, this is not the restaurant
     public:
+    // when making a poke bowl, you choose size which determines the amount of protein.
+    // when you choose the size, you choose your base.
+    // after your base, you choose topping
+    // after toppings, you choose your protein(s) then sauce
+    // top it off at the end
 
-    private:
+    //size S M L
+    // base: white rice, brown rice, spring mix
+    // topping: crabmeat, spicy crabmeat, seaweed salad, cucumber, edamame, corn, onions, mango, pickled radish, avacado
+    // protein: salmon, spicy salmon, tuna, spicy tuna, octopus, shrimp, scallop, yellowtail
+    // sauce: soy sauce, ponzu, spicy mayo, eel sauce, spicy ponzu
+    // top off: fish eggs, cilantro, green onions, pineapple, wasabi, ginger, furikake, sesame seeds, fried shallots
+
+
+
+    pokeBowl(); // completely new bowl, empty not chosen
+
+    void pickSize();
+    void pickBase();
+    void pickTopping();
+    void pickProteins();
+    void pickSauce();
+    void topItOff();
+    std::string printOrder();
+    int calculateOrder();
+
+
+    private: // these come from RNG, DONT PUT THEM IN THE CONSTRUCTOR.
+    std::string base_;
+    std::string protein_;
+    std::string topping_;
+    std::string top_off_;
+    std::string sauce_;
 };
 
 #endif

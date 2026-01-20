d19d18cb3eff486b068a3b8e1c6dfae5e45b9a4c
Lana Vu <lanavu@csu.fullerton.edu>
2026-01-20 14:52:35 -0800

    hashmap inside class, implemented so user can find orders if customer asks about it

--- Stores/RickRonalds.cpp
+++ Stores/RickRonalds.cpp
@@ -4,29 +4,31 @@
 #include <vector>
 #include <unordered_map>
 
-rickOrder::
+rickMenu::rickMenu() {
+    //structured as a whole order
+    items["Cheese Burger"] = 2;
+    items["Plain Burger"] = 2;
+    items["Rick-Double"] = 5.67;
+    items["Swordfish Burger"] = 2.5;
+    items["4 pc RickNuggets"] = 3;
+    items["10 pc rickNuggets"] = 4.5;
+    items["20pc rickNuggets"] = 6;
+    items["Small Fries"] = 3;
+    items["Medium Fries"] = 5.67;
+    items["Large Fries"] = 7;
+    items["Soda"] = 2;
+    items["Mango-banana Smoothie"] = 4.67;
+    items["Strawberry-banana Smoothie"] = 4.67;
+    items["Rick-Flurry"] = 4.50;
+}
 void rickPath() {
     std::unordered_map<std::string, double> rickMenu;
 
-    rickMenu["Cheese Burger"] = 2;
-    rickMenu["Plain Burger"] = 2;
-    rickMenu["Rick-Double"] = 5.67;
-    rickMenu["Swordfish Burger"] = 2.5;
-    rickMenu["4 pc RickNuggets"] = 3;
-    rickMenu["10 pc rickNuggets"] = 4.5;
-    rickMenu["20pc rickNuggets"] = 6;
-    rickMenu["Small Fries"] = 3;
-    rickMenu["Medium Fries"] = 5.67;
-    rickMenu["Large Fries"] = 7;
-    rickMenu["Soda"] = 2;
-    rickMenu["Mango-banana Smoothie"] = 4.67;
-    rickMenu["Strawberry-banana Smoothie"] = 4.67;
-    rickMenu["Rick-Flurry"] = 4.50;
 
-    std::cout << "This is our menu: ";
+    /*std::cout << "This is our menu: ";
 
     for (auto i : rickMenu) {
         std::cout << i.first << " $" << i.second;
     }
-
+*/
 }
--- Stores/RickRonalds.hpp
+++ Stores/RickRonalds.hpp
@@ -3,13 +3,18 @@
 #include <iostream>
 #include <vector>
 
-class rickOrder {
+class rickMenu {
     public:
-    rickOrder();
-    std::string npcOrdering();
+
+    rickMenu();
+    std::string npcOrderBurger();
+    std::string npcOrderNuggets();
     std::string npcLines();
+    // method 1: choices from 1-3
+    // other function determines if 1 then this type, if 2 then this type 
 
     private:
+    std::unordered_map<std::string, double> items;
     std::vector<std::string> burger_;
     std::vector<std::string> nuggets_;
     std::vector<std::string> fries_;

002e1746cefb4d311ababa14c14045945f100350
Lana Vu <lanavu@csu.fullerton.edu>
2026-01-20 20:31:24 -0800

    idfk

--- Stores/RickRonalds.hpp
+++ Stores/RickRonalds.hpp
@@ -7,21 +7,21 @@ class rickMenu {
     public:
 
     rickMenu();
-    std::string npcOrderBurger();
-    std::string npcOrderNuggets();
-    std::string npcLines();
+    
     // method 1: choices from 1-3
     // other function determines if 1 then this type, if 2 then this type 
 
     private:
     std::unordered_map<std::string, double> items;
-    std::vector<std::string> burger_;
-    std::vector<std::string> nuggets_;
-    std::vector<std::string> fries_;
-    std::vector<std::string> drink_;
-    bool iceCream_;
-    std::vector<std::string> order_;
 
 };
 
+class NPCOrder {
+    public:
+
+    //RNG ORDER
+    
+    private:
+    std::vector<rickMenu> bag;
+};
 #endif

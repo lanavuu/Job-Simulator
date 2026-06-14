82aa88e4ca3aa9a023c55391ffa6b4db9d5a1a1e
Lana Vu <lananghivu@gmail.com>
2026-06-13 22:30:24 -0700

    Removed third store to focus on first two, fixing game loop

--- CenterConsole.cpp
+++ CenterConsole.cpp
@@ -1,4 +1,4 @@
-#include "Stores/OutAndIn.hpp"
+
 #include "Stores/RickRonalds.hpp"
 #include "Stores/SuperPoke.hpp"
 #include "Exceptions.hpp"
@@ -8,6 +8,7 @@
 
 int main() {
     srand(time(nullptr)); //rand seed ONCE in main
+    while (true){
 
     std::cout << "Hey kid. You've got no money, no talent, no potential, and your parents hate you.\n";
     std::cout << "Imma take you in, but if you don't get no job then you're out of here.\n";
@@ -15,23 +16,21 @@ int main() {
 
     int choice;
     std::cout << "Apply to: \n";
-    std::cout << "1. Out-and-in, $24/hr\n";
-    std::cout << "2. RickRonalds, $20/hr\n";
-    std::cout << "3. Super-Poke, $16.50/hr\n";
+    std::cout << "1. RickRonalds, $20/hr\n";
+    std::cout << "2. Super-Poke, $16.50/hr\n";
+    std::cout << "3. Quit\n";
     std::cout << "Enter number: \n";
     std::cin >> choice;
 
     if (choice == 1) {
-        outPath();
-    } else if (choice == 2) {
         rickPath();
-    } else if (choice == 3) {
+    } else if (choice == 2) {
         pokePath();
+    } else if (choice == 3){
+        break;
     } else {
-        std::cout << "Invalid, choose either 1, 2, or 3."; // back
+        std::cout << "Invalid, choose either 1-3.\n"; // back
     }
-
-
-
     return 0;
+}
 }
